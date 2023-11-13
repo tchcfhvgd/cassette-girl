fa90fc2ef1b02e35bd9aedcc279f5a066d8b965a
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-11-13 14:40:48 +0800

    Update OptionsState.hx

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -39,6 +39,9 @@ class OptionsState extends MusicBeatState
 	var backdrops:FlxBackdrop = new FlxBackdrop(Paths.image('backdrop2'), 0.2, 0.2, true, true);
 
 	override function create() {
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+		
 		#if desktop
 		DiscordClient.changePresence("Options Menu", null);
 		#end
